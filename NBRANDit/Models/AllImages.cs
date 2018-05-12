97559d4e63b8f6d8d5ac128eca97a68158dde3ae
Jakub Jirkal <jirkal.jakub@gmail.com>
2018-05-12 19:12:35 +0200

    Backend DONE

--- NBRANDit/Models/AllImages.cs
+++ NBRANDit/Models/AllImages.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 public class AllImages{
     public List<string> Images;
+    public List<string> vals;
 
     public void Generate(){
         Images = new List<string>();
@@ -9,5 +12,13 @@ public class AllImages{
         Images.Add("2.jpg");
         Images.Add("3.jpg");
         Images.Add("4.jpg");
+    }
+
+    public void Calc(){
+        vals = new List<string>();
+        vals.Add("95%");
+        vals.Add("46% (same text)");
+        vals.Add("31% (partly text)");
+        vals.Add("11%");
     }
 }
